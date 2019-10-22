cb33a62bcae8b86dc141873c476b39fbf1ad6d32
sumeilan <912041511@qq.com>
2019-10-22 21:39:13 +0800

    10.22

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 import time
 import os
 
-from base import HTMLTestRunner
+from base import HTMLTestRunnerCN
 import unittest
 from email.mime.text import MIMEText
 from email.header import Header
@@ -54,7 +54,7 @@ if __name__ == '__main__':
     now = time.strftime("%Y-%m-%d %H_%M_%S")
     filename = root + '\\report\\' + now + '_result.html'
     fp = open(filename, "wb")
-    runner = HTMLTestRunner.HTMLTestRunner(
+    runner = HTMLTestRunnerCN.HTMLTestReportCN(
         stream=fp, title='接口自动构建测试报告', description='测试结果如下')
     runner.run(discover)
     fp.close()
