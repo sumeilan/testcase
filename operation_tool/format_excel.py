36f7b38a1a6c67243e8821dc9bef8804d7d157c6
sumeilan <912041511@qq.com>
2020-04-10 18:47:27 +0800

    20200410

--- operation_tool/format_excel.py
+++ operation_tool/format_excel.py
@@ -6,15 +6,18 @@ file = 'E:\\python\\testcase\\data\\wo.xlsx'
 workbook = openpyxl.load_workbook(filename=file)
 sheets = workbook.worksheets
 
-index = 0
+
 for sheet in sheets:
+    index = 0
     rows = sheet.rows
     sheet.row_dimensions[index].height = 40
     sheet.column_dimensions['F'].width = 30
-    font = Font(name='黑体', size=18, bold=False, italic=False, color='000000')
-    for cell in list(sheet.rows)[index]:
-        cell.font = font
-        print(cell)
-    index = index + 1
+    font = Font(name='黑体', size=10, bold=False, italic=False, color='000000')
+    for i in range(index,len(sheets)):
+        for cell in list(sheet.rows)[index]:
+            cell.font = font
+            print(cell)
+        index = index + 1
+
 
 workbook.save(file)
